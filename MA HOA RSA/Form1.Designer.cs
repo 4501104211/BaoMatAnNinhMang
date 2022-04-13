7b391a4f99f2808dcf4b6d615b3aa1c9aa83f9e1
tieutuan <tieuhoangtuan822001@gmail.com>
2022-04-13 11:54:28 +0700

    Ma hoa RSA

---
+++ MA HOA RSA/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtPlainText = new System.Windows.Forms.TextBox();
+            this.txtCipherText = new System.Windows.Forms.TextBox();
+            this.txtDecryptText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bản rõ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bản mã";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 314);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Giải mã";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(496, 29);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 3;
+            this.btnEncrypt.Text = "Mã hóa";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(496, 165);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 4;
+            this.btnDecrypt.Text = "Giải mã";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtPlainText
+            // 
+            this.txtPlainText.Location = new System.Drawing.Point(29, 55);
+            this.txtPlainText.Multiline = true;
+            this.txtPlainText.Name = "txtPlainText";
+            this.txtPlainText.Size = new System.Drawing.Size(759, 81);
+            this.txtPlainText.TabIndex = 5;
+            // 
+            // txtCipherText
+            // 
+            this.txtCipherText.Location = new System.Drawing.Point(29, 191);
+            this.txtCipherText.Multiline = true;
+            this.txtCipherText.Name = "txtCipherText";
+            this.txtCipherText.Size = new System.Drawing.Size(759, 81);
+            this.txtCipherText.TabIndex = 6;
+            // 
+            // txtDecryptText
+            // 
+            this.txtDecryptText.Location = new System.Drawing.Point(29, 333);
+            this.txtDecryptText.Multiline = true;
+            this.txtDecryptText.Name = "txtDecryptText";
+            this.txtDecryptText.Size = new System.Drawing.Size(759, 81);
+            this.txtDecryptText.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtDecryptText);
+            this.Controls.Add(this.txtCipherText);
+            this.Controls.Add(this.txtPlainText);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox txtPlainText;
+        private System.Windows.Forms.TextBox txtCipherText;
+        private System.Windows.Forms.TextBox txtDecryptText;
+    }
+}
+
---
+++ MA HOA RSA/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class RSAEncryption
+        {
+            #region Ham_MaHoa_GiaiMa
+            public static byte[] MaHoa(byte[] dulieu, RSAParameters RSAKey, bool doOAEF)
+            {
+                try
+                {
+                    RSACryptoServiceProvider rsa = new
+                    RSACryptoServiceProvider();
+                    rsa.ImportParameters(RSAKey);
+                    return rsa.Encrypt(dulieu, doOAEF);
+                }
+                catch (CryptographicException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return null;
+                }
+            }
+            public static byte[] GiaiMa(byte[] dulieu, RSAParameters RSAKey, bool doOAEF)
+            {
+                try
+                {
+                    RSACryptoServiceProvider rsa = new
+                    RSACryptoServiceProvider();
+                    rsa.ImportParameters(RSAKey);
+                    return rsa.Decrypt(dulieu, doOAEF);
+                }
+                catch (CryptographicException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return null;
+                }
+            }
+            #endregion
+        }
+
+        RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+        byte[] plainText;
+        byte[] cipherText;
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            plainText = Encoding.UTF8.GetBytes(txtPlainText.Text);
+            cipherText = RSAEncryption.MaHoa(plainText, rsa.ExportParameters(false), false);
+            StringBuilder sbHash = new StringBuilder();
+            foreach (byte b in cipherText)
+                sbHash.Append(String.Format("{0:x2}", b));
+            txtCipherText.Text = sbHash.ToString();
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            byte[] decryptText = RSAEncryption.GiaiMa(cipherText, rsa.ExportParameters(true), false);
+            txtDecryptText.Text = Encoding.UTF8.GetString(decryptText);
+        }
+    }
+}
